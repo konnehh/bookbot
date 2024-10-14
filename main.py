d743dc7e25aa8464334f4035b844625158e22f8f
konnehh <konrad@mllr.me>
2024-10-14 13:22:40 +0200

    counting words, letters and outputting characters sorted

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    book = "books/frankenstein.txt"
+    with open(book) as f:
         book_content = f.read()
-        print(book_content)
+        wordcount = count_words(book_content)
+        lettercount = sort_and_filter_chars(count_chars(book_content))
+        print(f"--- Begin report of {book} ---")
+        print(f"{wordcount} words found in the document")
+
+        for item in lettercount:
+            print(f"The '{item["char"]}' character was found {item["count"]} times")
+
+        print("--- End report ---")
 
 def count_words(string):
     return len(string.split())
+
+def count_chars(string):
+    result = {}
+    for i in string.lower():
+        if i in result.keys():
+            result[i] += 1
+        else:
+            result[i] = 1
+    return result
+
+def sort_on(dict):
+    return dict["count"]
+    
+
+def sort_and_filter_chars(dict):
+    result = []
+    for i in dict:
+        if i.isalpha():
+            result.append({"char": i, "count": dict[i]})
+    result.sort(reverse=True, key=sort_on)
+    return result
+
+
+
+
 
 main()
